437cd8e584f761157344c193461a63d8070e3299
“dongyuekai” <457545648@qq.com>
2021-05-07 14:46:28 +0800

    客户端路由

--- react-ssr/src/client/index.js
+++ react-ssr/src/client/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from '../share/pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+import routes from '../share/routes'
 
 // hydrate在二次渲染的时候会复用dom节点
-ReactDOM.hydrate(<Home />, document.getElementById('root'))
+// ReactDOM.hydrate(<Home />, document.getElementById('root'))
+// 客户端路由
+ReactDOM.hydrate(<BrowserRouter>{renderRoutes(routes)}</BrowserRouter>, document.getElementById('root'))
--- react-ssr/src/share/pages/Home.js
+++ react-ssr/src/share/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div onClick={() => { console.log('hello') }}>Home works---</div>
+    <div>
+      <div onClick={() => { console.log('hello') }}>Home works---</div>
+      <Link to='/list'>jump to list</Link>
+    </div>
   )
 }
 export default Home

5562a382777edde0e935393a55d78eb6223c0691
“dongyuekai” <457545648@qq.com>
2021-05-08 10:42:23 +0800

    预渲染（静态生成和服务器端渲染）

--- next/next-guide/pages/list.js
+++ next/next-guide/pages/list.js
@@ -1,14 +1,43 @@
 import Head from 'next/head'
 import styles from '../styles/list.module.css'
+import { readFile } from 'fs'
+import { promisify } from 'util'
+import { join } from 'path'
 
+const read = promisify(readFile)
 
-export default () => {
+export default ({ data }) => {
   return (
     <>
       <Head>
         <title>list page</title>
       </Head>
       <div className={styles.demo}>list</div>
+      <div>{data}</div>
     </>
   )
+}
+// 静态生成
+// getStaticProps会在构建的时候运行 在node环境下输出
+// export async function getStaticProps() {
+//   let data = await read(join(process.cwd(), 'pages', '_app.js'), 'utf-8')
+//   console.log('dyk---data----', data)
+//   return {
+//     props: {
+//       data
+//     }
+//   }
+// }
+
+// 服务器端渲染
+// 每次客户端向服务端发送请求的时候都会执行
+export async function getServerSideProps(context) {
+  console.log('dyk--', context.query)
+  let data = await read(join(process.cwd(), 'pages', '_app.js'), 'utf-8')
+  console.log('dyk---hello----')
+  return {
+    props: {
+      data
+    }
+  }
 }

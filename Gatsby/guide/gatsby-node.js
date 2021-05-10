b5cc5ca1177a8ad21981662150a1cb428456fba2
“dongyuekai” <457545648@qq.com>
2021-05-10 16:31:15 +0800

    根据slug构建文章详情页

--- Gatsby/guide/gatsby-node.js
+++ Gatsby/guide/gatsby-node.js
@@ -1,33 +1,77 @@
-// 创建页面
-function createPages({actions}){
+const path = require('path')
+
+// 以编程方式创建页面
+// function createPages({ actions }) {
+//   const { createPage } = actions
+//   // 获取模板的绝对路径
+//   const template = require.resolve("./src/templates/person.js")
+//   // 获取模板所需要的数据
+//   const persons = [
+//     {
+//       slug: "zhangsan",
+//       name: "张三",
+//       age: 20
+//     },
+//     {
+//       slug: "lisi",
+//       name: "李四",
+//       age: 23
+//     }
+//   ]
+//   // 根据模板和数据创建页面
+//   persons.forEach(person => {
+//     createPage({
+//       // 模板绝对路径
+//       component: template,
+//       // 访问地址
+//       path: `/person/${person.slug}`,
+//       // 传递给模板的数据
+//       context: person
+//     })
+//   })
+// }
+
+async function createPages({ graphql, actions }) {
   const { createPage } = actions
-  // 获取模板的绝对路径
-  const template = require.resolve("./src/templates/person.js")
-  // 获取模板所需要的数据
-  const persons = [
-    {
-      slug: "zhangsan",
-      name: "张三",
-      age: 20
-    },
-    {
-      slug: "lisi",
-      name: "李四",
-      age: 23
+  // 1 获取模板的绝对路径
+  const template = require.resolve('./src/templates/article.js')
+  // 2 获取模板所需要的数据
+  let { data } = await graphql(`
+    query  {
+      allMarkdownRemark {
+        nodes {
+         fields {
+          slug
+        }
+      }
     }
-  ]
-  // 根据模板和数据创建页面
-  persons.forEach(person=>{
+  }
+`)
+  // 3 根据模板和数据创建页面
+  data.allMarkdownRemark.nodes.forEach(node => {
     createPage({
-      // 模板绝对路径
       component: template,
-      // 访问地址
-      path: `/person/${person.slug}`,
-      // 传递给模板的数据
-      context: person
+      path: `/article/${node.fields.slug}`,
+      context: {
+        slug: node.fields.slug
+      }
     })
   })
 }
+
+// 以编程方式为所有md数据节点添加slug属性
+function onCreateNode({ node, actions }) {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
+  }
+}
 module.exports = {
-  createPages
+  createPages,
+  onCreateNode
 }
---
+++ Gatsby/guide/src/templates/article.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  graphql
+} from 'gatsby'
+
+export default function Article({ data }) {
+  console.log('dyk---', data)
+  return (
+    <div>
+      <p>{data.markdownRemark.frontmatter.title}</p>
+      <p>{data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
+    </div>
+  )
+}
+export const query = graphql`
+  query ($slug: String) {
+    markdownRemark(fields: {slug: {eq: $slug} }) {
+      html
+      frontmatter {
+        title
+        date
+      }
+      id
+   }
+ }
+`

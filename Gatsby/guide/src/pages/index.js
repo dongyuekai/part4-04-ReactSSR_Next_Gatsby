f840dc9964bfa871c3c67ad5accb543683cb18dc
“dongyuekai” <457545648@qq.com>
2021-05-10 13:32:29 +0800

    Link组件使用

--- Gatsby/guide/src/pages/index.js
+++ Gatsby/guide/src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Link to='/list'>jump to list</Link>
+    </div>
+  )
 }

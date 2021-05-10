bfab80d82418d7be1e1bbfe891f1aa44e778641e
“dongyuekai” <457545648@qq.com>
2021-05-10 16:57:16 +0800

    创建cms系统（strapi）

---
+++ Gatsby/cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b74682ac3905f5c23ad2072bf06a4f58'),
+    },
+  },
+});
---
+++ Gatsby/cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ad7d96bf-c628-4168-9ed6-78dff5ebcd7d'
+};
